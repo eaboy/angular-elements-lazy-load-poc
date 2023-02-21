46ff57d6144a365e9b25e33a960a07dfed55c056
Eduardo Aboy <eduardo.aboy@rallyon.com>
2023-02-21 13:57:19 +0100

    Update main module to load components sync & async

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { createNgModule, DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { ComponentTwoComponent } from './component-two/component-two.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const component2 = createCustomElement(ComponentTwoComponent, { injector: this.injector });
+    customElements.define('component-two', component2);
+    setTimeout(() => this.loadComponent(), 5000);
+  }
+
+  loadComponent() {
+    import('./component-one/component-one.module').then((m) => {
+      createNgModule(m.ComponentOneModule, this.injector)
+      console.log('Component 2 loaded');
+    })
+  }
+
+}
